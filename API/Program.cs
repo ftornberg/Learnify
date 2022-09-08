03bb5f92666cb853262d50d14f73343d7845d5de
Fredrik Törnberg <fredrik.tornberg.syne21lin@tucsweden.se>
2022-09-08 10:32:56 +0200

    Main async and database seeded.

--- API/Program.cs
+++ API/Program.cs
@@ -14,22 +14,25 @@ namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
             using var scope = host.Services.CreateScope();
             var services = scope.ServiceProvider;
+                
+            var logger = services.GetRequiredService<ILogger<Program>>();
             try {
 
                 var context = services.GetRequiredService<StoreContext>();
-                context.Database.Migrate();
+                await context.Database.MigrateAsync();
+                await StoreContextSeed.SeedAsync(context, logger);
+            
             }
             catch (Exception ex)
             {
-                var logger = services.GetRequiredService<ILogger<Program>>();
                 logger.LogError(ex, "An error occured during migration");
             }
-            host.Run();
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
