79dc0e7b39ab160d9a632de0fbb2a76bb85cebde
Fredrik Törnberg <fredrik.tornberg.syne21lin@tucsweden.se>
2022-09-08 16:29:39 +0200

    Added axios, hook for courses and service for CORS.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/courses").then((response) => {
+      console.log(response);
+      setCourses(response.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {courses.map((course: any, index) => {
+            return (
+              <li key={index}>
+                {course.id}
+                {course.title}
+              </li>
+            )
+        })}
+      </ul>
     </div>
   );
 }
