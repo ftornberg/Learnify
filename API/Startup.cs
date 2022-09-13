e9b8eb8d0ffe098d3cb7181f286e0f1b13ab5b1d
Fredrik Törnberg <fredrik.tornberg.syne21lin@tucsweden.se>
2022-09-13 20:06:00 +0200

    Implemented generic repository and interface

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,7 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<ICourseRepository, CourseRepository>();
+            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
             services.AddAutoMapper(typeof(MappingProfile));
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddControllers();
---
+++ Infrastructure/GenericRepository.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Entity.Interfaces;
+
+namespace Infrastructure
+{
+    public class GenericRepository<T> : IGenericRepository<T>
+    {
+        public Task<T> GetByIdAsync(dynamic id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
