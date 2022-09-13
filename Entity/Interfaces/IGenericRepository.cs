7f54a8ab5e6d6d3f994cfa1116e71b4dc5fc732e
Fredrik Törnberg <92160597+ftornberg@users.noreply.github.com>
2022-09-13 22:06:06 +0200

    Revert "Module 4/specifications"

--- Entity/Interfaces/IGenericRepository.cs
+++ Entity/Interfaces/IGenericRepository.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Entity.Specifications;
 
 namespace Entity.Interfaces
 {
@@ -9,9 +8,5 @@ namespace Entity.Interfaces
          Task<IReadOnlyList<T>> ListAllAsync();
 
          Task<T> GetByIdAsync(dynamic id);
-
-         Task<T> GetEntityWithSpec(ISpecification<T> spec);
-
-         Task<IReadOnlyList<T>> ListWithSpec(ISpecification<T> spec);
     }
 }
