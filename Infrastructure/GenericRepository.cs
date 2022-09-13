4e346940e1a4a9e0a7d4dcb34e17fafa702f076c
Fredrik Törnberg <92160597+ftornberg@users.noreply.github.com>
2022-09-13 22:30:24 +0200

    Revert "Revert "Module 4/specifications""

--- Infrastructure/GenericRepository.cs
+++ Infrastructure/GenericRepository.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Entity.Interfaces;
+using Entity.Specifications;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure
@@ -18,9 +20,24 @@ namespace Infrastructure
             return await _context.Set<T>().FindAsync(id);
         }
 
+        public async Task<T> GetEntityWithSpec(ISpecification<T> spec)
+        {
+            return await ApplySpec(spec).FirstOrDefaultAsync();
+        }
+
         public async Task<IReadOnlyList<T>> ListAllAsync()
         {
             return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> ListWithSpec(ISpecification<T> spec)
+        {
+            return await ApplySpec(spec).ToListAsync();
+        }
+
+        private IQueryable<T> ApplySpec(ISpecification<T> spec)
+        {
+            return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
         }
     }
 }
