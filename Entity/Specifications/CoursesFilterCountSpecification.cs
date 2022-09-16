9109fc26bfd0c26f7acdbb78a5ae56cdc6cde095
Fredrik Törnberg <fredrik.tornberg.syne21lin@tucsweden.se>
2022-09-16 23:32:29 +0200

    added CoursesFiltersCountSpecification

---
+++ Entity/Specifications/CoursesFilterCountSpecification.cs
@@ -0,0 +1,11 @@
+namespace Entity.Specifications
+{
+    public class CoursesFilterCountSpecification :BaseSpecification<Course>
+    {
+        public CoursesFilterCountSpecification(CourseParams courseParams) : base(x => 
+        !courseParams.CategoryId.HasValue || x.CategoryId == courseParams.CategoryId)
+        {
+
+        }
+    }
+}
