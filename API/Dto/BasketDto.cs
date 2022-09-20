4c9b19e20f0cac55d5aa6f1738cf1ee40dc57ab4
Fredrik Törnberg <fredrik.tornberg.syne21lin@tucsweden.se>
2022-09-20 21:36:57 +0200

    Added AllowCredentials to startup. + created RemoveBasket method + created MappingProfile for Basket, BasketDto and BasketItem, BasketItemDto. + Created BasketDto and BasketItemDto

---
+++ API/Dto/BasketDto.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace API.Dto
+{
+    public class BasketDto
+    {
+        public string ClientId { get; set; }
+        public List<BasketItemDto> Items { get; set; }
+    }
+}
