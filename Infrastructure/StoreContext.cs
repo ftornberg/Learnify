aa988185874a4293fcdf20325a5ac14b96d93b18
Fredrik Törnberg <fredrik.tornberg.syne21lin@tucsweden.se>
2022-09-20 19:27:08 +0200

    Created Basket and BasketItem, added basket in StoreContext

--- Infrastructure/StoreContext.cs
+++ Infrastructure/StoreContext.cs
@@ -12,9 +12,9 @@ namespace Infrastructure
 
         public DbSet<Course> Courses { get; set; }
         public DbSet<Category> Categories { get; set; }
-
-        public DbSet<Requirement> Requirements { get; set; }
         public DbSet<Learning> Learnings { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
