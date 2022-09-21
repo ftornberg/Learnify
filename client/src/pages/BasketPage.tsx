fefbffdfa6f4b276ac0d7cf59ab3ab3074e0514b
Fredrik Törnberg <fredrik.tornberg.syne21lin@tucsweden.se>
2022-09-21 14:28:56 +0200

    wip: + added columns and import + added basket to main.scss + created _basket.scss

--- client/src/pages/BasketPage.tsx
+++ client/src/pages/BasketPage.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { Table } from 'antd';
 import agent from '../actions/agent';
 import { Basket } from '../models/basket';
+import * as FaIcons from 'react-icons/fa';
+import { Course } from '../models/course';
 
 const BasketPage = () => {
 	const [items, setItems] = useState<Basket>();
@@ -10,9 +13,48 @@ const BasketPage = () => {
 			setItems(response);
 		});
 	}, []);
+
+	const columns = [
+		{
+			title: 'Image',
+			dataIndex: 'image',
+			key: 'image',
+			render: (url: string) => {
+				return <img width="100px" src={url} alt={url} />;
+			},
+		},
+		{
+			title: 'Title',
+			dataIndex: 'title',
+			key: 'title',
+		},
+		{
+			title: 'Price',
+			dataIndex: 'price',
+			key: 'price',
+		},
+		{
+			title: 'Instructor',
+			key: 'instructor',
+			dataIndex: 'instructor',
+		},
+		{
+			title: 'Action',
+			key: 'action',
+			render: (item: Course) => (
+				<div>
+					<FaIcons.FaTrash />
+				</div>
+			),
+		},
+	];
+
 	return (
 		<>
-			<h1>{items?.clientId}</h1>
+			<div className="basket-page">
+				<h1 className="basket-page__header">Shopping Cart</h1>
+				<Table columns={columns} dataSource={items?.items} />
+			</div>
 		</>
 	);
 };
