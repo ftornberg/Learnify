214f02f68ed83e93720437b6b1b2195b5ac60ee8
Fredrik Törnberg <fredrik.tornberg.syne21lin@tucsweden.se>
2022-09-22 16:16:58 +0200

    Added getCourseAsync in courseSlice.ts implemented in Homepage.tsx

--- client/src/redux/slice/courseSlice.ts
+++ client/src/redux/slice/courseSlice.ts
@@ -6,19 +6,20 @@ import {
 import agent from '../../actions/agent';
 import { Course } from '../../models/course';
 import { PaginatedCourse } from '../../models/paginatedCourse';
+import { RootState } from '../store/configureStore';
 
 const coursesAdapter = createEntityAdapter<Course>();
 
-const getCoursesAsync = createAsyncThunk<PaginatedCourse | undefined, void>(
-	'course/getCoursesAsync',
-	async () => {
-		try {
-			return await agent.Courses.list();
-		} catch (error) {
-			console.log(error);
-		}
+export const getCoursesAsync = createAsyncThunk<
+	PaginatedCourse | undefined,
+	void
+>('course/getCoursesAsync', async () => {
+	try {
+		return await agent.Courses.list();
+	} catch (error) {
+		console.log(error);
 	}
-);
+});
 
 export const courseSlice = createSlice({
 	name: 'course',
@@ -40,3 +41,7 @@ export const courseSlice = createSlice({
 		});
 	},
 });
+
+export const coursesSelector = coursesAdapter.getSelectors(
+	(state: RootState) => state.course
+);
