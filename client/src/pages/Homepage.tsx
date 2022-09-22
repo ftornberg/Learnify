6602557eaae71871c079e3cfc6eb5af46ad86d70
Fredrik Törnberg <fredrik.tornberg.syne21lin@tucsweden.se>
2022-09-22 17:56:04 +0200

    WIP: dinner.

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { Course } from '../models/course';
 import ShowCourses from '../component/ShowCourses';
-import { Row } from 'antd';
+import { Card, Col, Radio, Row } from 'antd';
 import { useAppDispatch, useAppSelector } from '../redux/store/configureStore';
 import { coursesSelector, getCoursesAsync } from '../redux/slice/courseSlice';
 
@@ -21,10 +21,21 @@ const Homepage = () => {
 				<h2>New Courses Just For You...</h2>
 			</div>
 			<Row gutter={[24, 32]}>
-				{courses &&
-					courses.map((course: Course, index: number) => {
-						return <ShowCourses key={index} course={course} />;
-					})}
+				<Col span={4}>
+					<Card title="Sorting Oprions">
+						<Radio.Group
+						// TIMESTAMP @ 02:15, video: 9.14
+						/>
+					</Card>
+				</Col>
+				<Col span={20}>
+					<Row gutter={[24, 32]}>
+						{courses &&
+							courses.map((course: Course, index: number) => {
+								return <ShowCourses key={index} course={course} />;
+							})}
+					</Row>
+				</Col>
 			</Row>
 		</div>
 	);
