ed4258f659e2f000375ccdebee134bb71e64e07c
Fredrik Törnberg <fredrik.tornberg.syne21lin@tucsweden.se>
2022-09-24 10:49:07 +0200

    implemented authentication metods for validating token

--- API/Controllers/ErrorsController.cs
+++ API/Controllers/ErrorsController.cs
@@ -1,4 +1,5 @@
 using Infrastructure;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -12,17 +13,25 @@ namespace API.Controllers
             _context = context;
         }
 
+        [HttpGet("authcheck")]
+        [Authorize]
+
+        public ActionResult<string> CheckAuthorization()
+        {
+            return "You are Authorized";
+        }
+
         [HttpGet("notFound")]
 
         public ActionResult NotFoundMethod()
         {
             var category = _context.Categories.Find(42);
 
-            if(category == null) return NotFound();
+            if (category == null) return NotFound();
 
             return Ok();
         }
-        
+
         [HttpGet("serverError")]
 
         public ActionResult ServerErrorMethod()
@@ -31,14 +40,14 @@ namespace API.Controllers
 
             return Ok(category.ToString());
         }
-        
+
         [HttpGet("badRequest")]
 
         public ActionResult badRequestMethod()
         {
             return BadRequest();
         }
-        
+
         [HttpGet("badRequest/{id}")]
 
         public ActionResult badIdMethod(int id)
