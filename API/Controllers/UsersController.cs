b880b9bc6a40adf814a7a912f20c324ec4867e5a
Fredrik Törnberg <fredrik.tornberg.syne21lin@tucsweden.se>
2022-09-26 20:57:08 +0200

    Created endpoint for current user. + refactoring

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -42,6 +42,8 @@ namespace API.Controllers
 
             var userBasket = await ExtractBasket(user.UserName);
             var basket = await ExtractBasket(Request.Cookies["clientId"]);
+            var courses = _context.UserCourses.AsQueryable();
+
 
             if (basket != null)
             {
@@ -55,6 +57,8 @@ namespace API.Controllers
                 Email = user.Email,
                 Token = await _tokenService.GenerateToken(user),
                 Basket = basket != null ? _mapper.Map<Basket, BasketDto>(basket) : _mapper.Map<Basket, BasketDto>(userBasket),
+                Courses = courses.Where(x => x.UserId == user.Id).Select(u => u.Course).ToList(),
+
             };
         }
 
@@ -104,6 +108,24 @@ namespace API.Controllers
             if (result) return Ok();
 
             return BadRequest(new ApiResponse(400, "Problem adding courses"));
+        }
+
+        [Authorize]
+        [HttpGet("currentuser")]
+
+        public async Task<ActionResult<UserDto>> GetCurrentUser()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var basket = await ExtractBasket(User.Identity.Name);
+            var courses = _context.UserCourses.AsQueryable();
+
+            return new UserDto
+            {
+                Email = user.Email,
+                Token = await _tokenService.GenerateToken(user),
+                Basket = _mapper.Map<Basket, BasketDto>(basket),
+                Courses = courses.Where(x => x.UserId == user.Id).Select(u => u.Course).ToList(),
+            };
         }
 
         private async Task<Basket> ExtractBasket(string clientId)
