71c843725bc7a119aa7fbab13e883a93a1981310
Fredrik Törnberg <fredrik.tornberg.syne21lin@tucsweden.se>
2022-09-27 16:00:42 +0200

    Created LecturesController and endpoints. + Created LectureDto, SectionDto and UserLectureDto. + Updated MappingProfile to include LectureDto and SectionDto

---
+++ API/Dto/SectionDto.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace API.Dto
+{
+    public class SectionDto
+    {
+        public string SectionName { get; set; }
+        public List<LectureDto> Lectures { get; set; }
+    }
+}
