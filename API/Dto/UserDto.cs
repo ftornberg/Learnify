bcea6e79239be18cf607ff7112edba14ea657888
Fredrik Törnberg <fredrik.tornberg.syne21lin@tucsweden.se>
2022-09-29 12:14:32 +0200

    Added instructor role to UserMenu and added claims. addeed role to user.ts

--- API/Dto/UserDto.cs
+++ API/Dto/UserDto.cs
@@ -6,11 +6,11 @@ using Entity;
 
 namespace API.Dto
 {
-    public class UserDto
-    {
-        public string Email { get; set; }
-        public string Token { get; set; }
-        public BasketDto Basket { get; set; }
-        public List<Course> Courses { get; set; }
-    }
+  public class UserDto
+  {
+    public string Email { get; set; }
+    public string Token { get; set; }
+    public BasketDto Basket { get; set; }
+    public List<Course> Courses { get; set; }
+  }
 }
--- client/src/redux/slice/userSlice.ts
+++ client/src/redux/slice/userSlice.ts
@@ -1,6 +1,5 @@
 import { createAsyncThunk, createSlice, isAnyOf } from '@reduxjs/toolkit';
 import { notification } from 'antd';
-import { FaLeaf } from 'react-icons/fa';
 import agent from '../../actions/agent';
 import { Course } from '../../models/course';
 import { Login, Register, User } from '../../models/user';
@@ -75,7 +74,13 @@ export const userSlice = createSlice({
 			localStorage.removeItem('user');
 		},
 		setUser: (state, action) => {
-			state.user = action.payload;
+			let claims = JSON.parse(atob(action.payload.token.split('.')[1]));
+			let roles =
+				claims['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'];
+			state.user = {
+				...action.payload,
+				roles: typeof roles === 'string' ? [roles] : roles,
+			};
 		},
 		setUserCourses: (state, action) => {
 			state.userCourses = action.payload;
@@ -96,7 +101,15 @@ export const userSlice = createSlice({
 				fetchCurrentUser.fulfilled
 			),
 			(state, action) => {
-				state.user = action.payload;
+				let claims = JSON.parse(atob(action.payload.token.split('.')[1]));
+				let roles =
+					claims[
+						'http://schemas.microsoft.com/ws/2008/06/identity/claims/role'
+					];
+				state.user = {
+					...action.payload,
+					roles: typeof roles === 'string' ? [roles] : roles,
+				};
 			}
 		);
 		builder.addMatcher(
