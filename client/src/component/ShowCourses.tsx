3818eb44c118b5d8b1e8eda7a137a6ee55f66164
Fredrik Törnberg <fredrik.tornberg.syne21lin@tucsweden.se>
2022-09-30 13:09:30 +0200

    fixes and trixes

--- client/src/component/ShowCourses.tsx
+++ client/src/component/ShowCourses.tsx
@@ -52,43 +52,50 @@ const ShowCourses = ({ course }: Props) => {
 	return (
 		<>
 			<Col className="gutter-row" span={spanVal}>
-				<Card
-					className="card"
-					hoverable
-					cover={<img width="100%" alt="course-cover" src={course.image} />}
-				>
-					<Link to={`/course/${course.id}`}>
-						<div className="course__title">{course.title}</div>
-					</Link>
-					<div className="course__instructor">{course.instructor}</div>
-					<div className="course__rating">
-						{course.rating}
-						<span>{showStars(course.rating)}</span>
-					</div>
-					<div className="course__bottom">
-						<div className="course__bottom__price">{course.price}</div>
-						{userCourses?.find((item: Course) => item.id === course.id) !==
-						undefined ? (
-							<Link to={`/learn/${course.id}/${currentLecture}`}>
-								<div className="course__bottom__cart">Go to Course</div>
-							</Link>
-						) : basket?.items.find((item) => item.courseId === course.id) !==
-						  undefined ? (
-							<Link to="/basket">
-								<div className="course__bottom__cart">Go to Cart</div>
-							</Link>
-						) : (
-							<div
-								onClick={() =>
-									dispatch(addBasketItemAsync({ courseId: course.id }))
-								}
-								className="course__bottom__cart"
-							>
-								Add to Cart
-							</div>
-						)}
-					</div>
-				</Card>
+				<div className="card">
+					<Card
+						cover={
+							<img
+								className="img"
+								width="100%"
+								alt="course-cover"
+								src={course.image}
+							/>
+						}
+					>
+						<Link to={`/course/${course.id}`}>
+							<div className="course__title">{course.title}</div>
+						</Link>
+						<div className="course__instructor">{course.instructor}</div>
+						<div className="course__rating">
+							{course.rating}
+							<span>{showStars(course.rating)}</span>
+						</div>
+						<div className="course__bottom">
+							<div className="course__bottom__price">{course.price}</div>
+							{userCourses?.find((item: Course) => item.id === course.id) !==
+							undefined ? (
+								<Link to={`/learn/${course.id}/${currentLecture}`}>
+									<div className="course__bottom__cart">Go to Course</div>
+								</Link>
+							) : basket?.items.find((item) => item.courseId === course.id) !==
+							  undefined ? (
+								<Link to="/basket">
+									<div className="course__bottom__cart">Go to Cart</div>
+								</Link>
+							) : (
+								<div
+									onClick={() =>
+										dispatch(addBasketItemAsync({ courseId: course.id }))
+									}
+									className="course__bottom__cart"
+								>
+									Add to Cart
+								</div>
+							)}
+						</div>
+					</Card>
+				</div>
 			</Col>
 		</>
 	);
